65a9c5fc6faa9d5c0a4556ce0e2acc03f2739359
dano does design <dano.does.design@gmail.com>
2022-04-18 03:57:23 +1000

    add link

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <div class="container flex flex-col gap-10 p-10 mx-auto">
-      <h1 class="text-5xl ">Scaffold</h1>
+      <h1 class="text-5xl font-bold">✨ Scaffold</h1>
       <p>A basic Vue 3 project scaffold with a few sprinkles of joy.</p>
+      <a class="text-blue-800 underline transition-all hover:underline-offset-2 hover:decoration-2 hover:text-black"
+        href="https://github.com/danodoesdesign/scaffold">View on Github</a>
       <hr />
     </div>
     <router-view></router-view>
