b73357ebf694ac5ee675022cc81b073986eddd22
dano does design <dano.does.design@gmail.com>
2022-04-17 23:38:53 +1000

    data example

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
-  <p>stuff goes here</p>
+  <ul>
+    <li class="mb-10" v-for="book in books">
+      <h5 class="text-4xl">{{ book.title }}</h5><br>
+      <p class="-mt-5">By {{ book.author }}</p>
+    </li>
+  </ul>
 </template>
 
-<script setup>
-
+<script>
+export default {
+  data() {
+    return {
+      data: null,
+      showBooks: true,
+      books: [
+        { title: 'The Catcher in the Rye', author: 'J.D. Salinger' },
+        { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+        { title: 'The Grapes of Wrath', author: 'John Steinbeck' },
+        { title: 'Dan is cool', author: 'Dan' },
+      ]
+    }
+  },
+};
 </script>
 
 <style>
